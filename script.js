46ab65eefa4886f1c8174b9ed49fa224e12b6f6c
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-06-12 21:12:40 +0530

    (feat) added logic to check no winner state, refactoring

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*
-* we are making a tic tac toe game
-*(done) we need a board with 9 cells - cells will be buttons 
-*(done) there should be a board object
-*(done)    board will have 3 rows  - each row 3 cells
-*(done) we need player objects : name, mark, score
-*(done) cell object will hold state : filled/empty
-*(done) cell object will hold mark/player id 
-* make a game controller
-* 1. controller will by default assign first turn to player 1
-* 2. when player clicks on a button, controller will mark it
-* 3. controller will check if the player has won, if not
-* 4a. controller will pass the turn to other player
-* 5. repeat step 2 onwards
-* 4b. if a player has won, the controller will display congrats
-*/
-
 function GameBoard() {
   const rows = 3;
   const columns = 3;
@@ -22,14 +5,11 @@ function GameBoard() {
   let row_sums = [0,0,0]; /* row-0, row-1, row-2 */
   let col_sums = [0,0,0]; /* col-0, col-1, col-2 */
   let diag_sums = [0,0];  /* diag-0 diag-1*/
-  /* horizontal/ diagonals / veritical */
-  /* const lines =[ ["00", "01", "02"], ["10", "11", "12"], ["20", "21", "22"], 
-                 ["00", "11", "22"], ["20", "11", "02"],  
-                 ["00", "10", "20"], ["01", "11", "21"], ["02", "12", "22"] ] */
+  let x_count = 0;
+  let o_count = 0;
   let play_area = document.createElement("div");
   document.querySelector('body').appendChild(play_area);
   play_area.setAttribute('class', 'play-area');
-  console.log(play_area);
 
   for (let i = 0; i < rows; i++){
     board[i] = [];
@@ -54,6 +34,7 @@ function GameBoard() {
       col_sums[cell] += player.weight;
       if (row==cell) {diag_sums[0]+= player.weight;}
       if (row+cell==2) {diag_sums[1]+= player.weight;}
+      (player.mark==='x') ? x_count++ : o_count++;
       /* html logic */
       let btn = document.getElementById(`btn-${row}${cell}`);
       btn.innerHTML = player.mark;
@@ -67,9 +48,13 @@ function GameBoard() {
     return {row_sums, col_sums, diag_sums}
   }
 
-  function getBoard(){
-    return board;
+  function noWinner() {
+    return (x_count + o_count === 9)
   }
+
+  // function getBoard(){
+  //   return board;
+  // }
 
   function displayBoard(){
     let row_val = " ";
@@ -83,7 +68,7 @@ function GameBoard() {
     }
   }
   return {
-    getBoard, displayBoard, markCell, getLineSums
+   markCell, getLineSums, displayBoard, noWinner
   }
 }
 
@@ -101,6 +86,8 @@ function createAndStyleButtons(i,j){
   btn.style.padding = "1rem";
   btn.style.margin = "0.5rem";
   btn.style.borderRadius = "10%";
+  btn.style.color = 'whitesmoke';
+  btn.style.backgroundColor = "whitesmoke";
   return {btn}
 }
 
@@ -141,7 +128,7 @@ function GameController() {
     return {row,col}
   }
 
-  board = GameBoard()
+  board = GameBoard();
   game_buttons = Array.from(document.querySelectorAll('button'));
   game_buttons.forEach(btn => 
     { btn.addEventListener("click",  (ev) => nextMove(ev));
@@ -150,9 +137,10 @@ function GameController() {
   function nextMove(ev){
     let ids = logId(ev);
     let valid_click = board.markCell(ids.row, ids.col, current_player);
+    board.displayBoard();
     /* check if player won */
     let win_flag = getWinningPlayer(current_player)
-    if (win_flag) { 
+    if (win_flag || board.noWinner() ) { 
       disableGame();
       return;}
     if (valid_click ) { switchPlayer()}
@@ -166,30 +154,25 @@ function GameController() {
   function getWinningPlayer(plr) {
     let adj_score = 3 * plr.weight;
     scores = board.getLineSums()
-    // console.log(scores.row_sums);
-    // console.log(scores.col_sums);
-    // console.log(scores.diag_sums);
     const score_match = (el) => el === adj_score;
     if(scores.row_sums.some(score_match)) {
-      console.log(`Player-${plr.id} won`);
-      console.log(`row-${scores.row_sums.indexOf(adj_score)}`);
-      let rows_to_color = Array.from(document.querySelectorAll(`.row-${scores.row_sums.indexOf(adj_score)}`));
-      rows_to_color.forEach(el => {el.style.backgroundColor = "green";})
+      declareVictory(`.row-${scores.row_sums.indexOf(adj_score)}`);
       return true;
     } else if(scores.col_sums.some(score_match)) {
-      console.log(`Player-${plr.id} won`);
-      console.log(`col-${scores.col_sums.indexOf(adj_score)}`);
-      let cols_to_color = Array.from(document.querySelectorAll(`.col-${scores.col_sums.indexOf(adj_score)}`));
-      cols_to_color.forEach(el => {el.style.backgroundColor = "green";})
+      declareVictory(`.col-${scores.col_sums.indexOf(adj_score)}`, plr.id);
       return true;
     } else if(scores.diag_sums.some(score_match)) {
-      console.log(`Player-${plr.id} won`);
-      console.log(`diag-${scores.diag_sums.indexOf(adj_score)}`);
-      let diag_to_color = Array.from(document.querySelectorAll(`.diag-${scores.diag_sums.indexOf(adj_score)}`));
-      diag_to_color.forEach(el => {el.style.backgroundColor = "green";})
+      declareVictory(`.diag-${scores.diag_sums.indexOf(adj_score)}`, plr.id);
       return true;
     }
     return false;
+  }
+
+  function declareVictory(identifier, pid){
+    console.log(`Player-${pid} won`);
+    console.log(identifier);
+    let cell_array = Array.from(document.querySelectorAll(identifier));
+      cell_array.forEach(el => {el.style.backgroundColor = "green";})
   }
 
   function switchPlayer() {
