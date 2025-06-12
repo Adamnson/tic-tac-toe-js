ad1463672809bb31117b9a1c4a44afed826f2b4c
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-06-12 19:47:42 +0530

    (feat) implement win logic

--- script.js
+++ script.js
@@ -19,6 +19,13 @@ function GameBoard() {
   const rows = 3;
   const columns = 3;
   const board = [];
+  let row_sums = [0,0,0]; /* row-0, row-1, row-2 */
+  let col_sums = [0,0,0]; /* col-0, col-1, col-2 */
+  let diag_sums = [0,0];  /* diag-0 diag-1*/
+  /* horizontal/ diagonals / veritical */
+  /* const lines =[ ["00", "01", "02"], ["10", "11", "12"], ["20", "21", "22"], 
+                 ["00", "11", "22"], ["20", "11", "02"],  
+                 ["00", "10", "20"], ["01", "11", "21"], ["02", "12", "22"] ] */
   let play_area = document.createElement("div");
   document.querySelector('body').appendChild(play_area);
   play_area.setAttribute('class', 'play-area');
@@ -27,7 +34,6 @@ function GameBoard() {
   for (let i = 0; i < rows; i++){
     board[i] = [];
     let temp_row = document.createElement("div");
-    temp_row.setAttribute("class", `row-${i}`);
 
     for (let j = 0; j < columns; j++){
       board[i].push(Cell());
@@ -39,13 +45,26 @@ function GameBoard() {
   function markCell(row, cell, player) {
     let cell_to_fill = board[row][cell];
     if (!cell_to_fill.filled) {
+      /* game objects logic */
       cell_to_fill.filled = true;
       cell_to_fill.mark = player.mark;
       cell_to_fill.player = player.id;
+      /* game score logic */
+      row_sums[row] += player.weight;
+      col_sums[cell] += player.weight;
+      if (row==cell) {diag_sums[0]+= player.weight;}
+      if (row+cell==2) {diag_sums[1]+= player.weight;}
+      /* html logic */
       let btn = document.getElementById(`btn-${row}${cell}`);
       btn.innerHTML = player.mark;
+      btn.style.color = player.color;
+      btn.style.backgroundColor = "blanchedalmond";
       return true;
     } else { return false}
+  }
+  
+  function getLineSums() {
+    return {row_sums, col_sums, diag_sums}
   }
 
   function getBoard(){
@@ -64,17 +83,24 @@ function GameBoard() {
     }
   }
   return {
-    getBoard, displayBoard, markCell
+    getBoard, displayBoard, markCell, getLineSums
   }
 }
 
 function createAndStyleButtons(i,j){
+  /* html logic */
   let btn = document.createElement("button");
+  btn.innerHTML = '-';
   btn.setAttribute('id',`btn-${i}${j}`);
+  btn.classList.add( `row-${i}`);
+  btn.classList.add(`col-${j}`);
+  if (i==j) {btn.classList.add('diag-0');}
+  if (i+j==2){btn.classList.add('diag-1');}
+  /* css logic */
   btn.style.minHeight = "2rem";
   btn.style.padding = "1rem";
   btn.style.margin = "0.5rem";
-  btn.innerHTML = '-';
+  btn.style.borderRadius = "10%";
   return {btn}
 }
 
@@ -92,12 +118,16 @@ function GameController() {
     {
       id: 1,
       mark: 'x',
-      name: 'Player 1'
+      name: 'Player 1',
+      color: "yellow",
+      weight: 1
     },
     {
       id: 2,
       mark: 'o',
-      name: 'Player 2'
+      name: 'Player 2',
+      color: "skyblue",
+      weight: -1
     }
   ]
 
@@ -120,7 +150,46 @@ function GameController() {
   function nextMove(ev){
     let ids = logId(ev);
     let valid_click = board.markCell(ids.row, ids.col, current_player);
-    if (valid_click) { switchPlayer()}
+    /* check if player won */
+    let win_flag = getWinningPlayer(current_player)
+    if (win_flag) { 
+      disableGame();
+      return;}
+    if (valid_click ) { switchPlayer()}
+  }
+
+  function disableGame() {
+    let btns = document.querySelectorAll("button");
+    btns.forEach ((el) => {el.disabled = true;})
+  }
+
+  function getWinningPlayer(plr) {
+    let adj_score = 3 * plr.weight;
+    scores = board.getLineSums()
+    // console.log(scores.row_sums);
+    // console.log(scores.col_sums);
+    // console.log(scores.diag_sums);
+    const score_match = (el) => el === adj_score;
+    if(scores.row_sums.some(score_match)) {
+      console.log(`Player-${plr.id} won`);
+      console.log(`row-${scores.row_sums.indexOf(adj_score)}`);
+      let rows_to_color = Array.from(document.querySelectorAll(`.row-${scores.row_sums.indexOf(adj_score)}`));
+      rows_to_color.forEach(el => {el.style.backgroundColor = "green";})
+      return true;
+    } else if(scores.col_sums.some(score_match)) {
+      console.log(`Player-${plr.id} won`);
+      console.log(`col-${scores.col_sums.indexOf(adj_score)}`);
+      let cols_to_color = Array.from(document.querySelectorAll(`.col-${scores.col_sums.indexOf(adj_score)}`));
+      cols_to_color.forEach(el => {el.style.backgroundColor = "green";})
+      return true;
+    } else if(scores.diag_sums.some(score_match)) {
+      console.log(`Player-${plr.id} won`);
+      console.log(`diag-${scores.diag_sums.indexOf(adj_score)}`);
+      let diag_to_color = Array.from(document.querySelectorAll(`.diag-${scores.diag_sums.indexOf(adj_score)}`));
+      diag_to_color.forEach(el => {el.style.backgroundColor = "green";})
+      return true;
+    }
+    return false;
   }
 
   function switchPlayer() {
